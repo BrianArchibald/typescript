1107887118cc3d533d32707cac6fb885f2665d6f
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-29 15:21:41 -0600

    Refactor: initial commit

---
+++ types.js
@@ -0,0 +1,2 @@
+// use tsc types.ts to compile to js file
+console.log('TS files');
---
+++ types.ts
@@ -0,0 +1,8 @@
+// use tsc types.ts to compile to js file
+// use tsc types.ts -w    for watch mode to compile automatically on changes
+
+let myString: string;
+myString = 'This is the way too'
+
+console.log(myString)
+
